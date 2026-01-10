93d84ea74eea4bb8f0e9838f87a65640708b3c0e
flaviomelian <flaviomelian2000@gmail.com>
2026-01-10 15:50:47 +0000

    Estructura inicial del proyecto creada: backend Spring Boot y frontend Next.js

---
+++ backend/src/test/java/com/solar_sim/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solar_sim.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
